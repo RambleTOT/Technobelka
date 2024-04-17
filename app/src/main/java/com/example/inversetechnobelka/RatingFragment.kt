3d2249f0fc10f05d828645f1f14bb4adc40ad79a
RambleTOT <rambletot@gmail.com>
2024-04-17 03:02:46 +0300

    ui rating

--- app/src/main/java/com/example/inversetechnobelka/RatingFragment.kt
+++ app/src/main/java/com/example/inversetechnobelka/RatingFragment.kt
@@ -1,59 +1,60 @@
 package com.example.inversetechnobelka
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.inversetechnobelka.databinding.FragmentHomeBinding
+import com.example.inversetechnobelka.databinding.FragmentRatingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RatingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RatingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentRatingBinding? = null
+    private var currentClickLayout = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rating, container, false)
+        binding = FragmentRatingBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RatingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RatingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
     }
+
+    private fun init(){
+        binding!!.clickLayoutOne.setOnClickListener {
+            if (currentClickLayout == 2){
+                binding!!.clickLayoutOne.setBackgroundResource(R.drawable.background_tapbar_rating)
+                binding!!.clickTextOne.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.white)))
+                binding!!.clickLayoutTwo.setBackgroundColor(Color.TRANSPARENT)
+                binding!!.clickTextTwo.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.color_text_title)))
+                binding!!.layoutFaculty.visibility = View.VISIBLE
+                binding!!.layoutPersonal.visibility = View.GONE
+                currentClickLayout = 1
+            }
+        }
+        binding!!.clickLayoutTwo.setOnClickListener {
+            if (currentClickLayout == 1) {
+                binding!!.clickLayoutTwo.setBackgroundResource(R.drawable.background_tapbar_rating)
+                binding!!.clickTextTwo.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.white)))
+                binding!!.clickLayoutOne.setBackgroundColor(Color.TRANSPARENT)
+                binding!!.clickTextOne.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(requireActivity(), R.color.color_text_title)))
+                binding!!.layoutFaculty.visibility = View.GONE
+                binding!!.layoutPersonal.visibility = View.VISIBLE
+                currentClickLayout = 2
+            }
+        }
+    }
+
 }
